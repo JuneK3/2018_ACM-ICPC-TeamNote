37c207a09309a3c1b24306e2acb1cb6c9a22a8d3
rlarudrms28 <rlarudrms28@gmail.com>
2020-02-06 04:40:00 +0000

    double_linked_list update

--- src/Data Structure/linked_list.cpp
+++ src/Data Structure/linked_list.cpp
@@ -3,7 +3,6 @@
 // 멤버함수 : push_front / push_back / pop_front / pop_back /
 // insert / erase...
 
-// 일단 단방향 링크드리스트로 구현
 #include <iostream>
 #include <cstdlib>
 using namespace std;
@@ -13,33 +12,53 @@ class InvalidIndexException{};
 template<typename T>
 class ListNode{
 public:
-    T value;
-    ListNode<T> *next; // 마지막 노드면 nullptr
- 
-    // 생성자
-    ListNode<T>(): next(nullptr){}
-    ListNode<T>(T value1, ListNode<T> *next1): value(value1), next(next1){}
+  T value;
+  ListNode<T> *next; // 마지막 노드면 nullptr
+  ListNode<T> *prev;
+  // 생성자
+  ListNode<T>(T value1, ListNode<T> *prev1, ListNode<T> *next1): value(value1), prev(prev1), next(next1){}
+  ListNode<T>(T value2): value(value2), prev(nullptr), next(nullptr){}
 };
  
 template<typename T>
 class LinkedList{
 public:
-    int size;
-    ListNode<T> *head;
+  int size;
+  ListNode<T> *head;
+  ListNode<T> *tail;
  
-    // 생성자
-    LinkedList<T>(): size(0), head(nullptr){}
+  // 생성자
+  LinkedList<T>(T value){
+    tail = head = new ListNode<T>(value);
+    size = 1;
+  }
  
-    // 소멸자
-    ~LinkedList<T>(){
-        ListNode<T> *t1 = head, *t2;
-        while(t1 != nullptr){
-            t2 = t1->next;
-            delete t1;
-            t1 = t2;
-        }
+  // 소멸자
+  ~LinkedList<T>(){
+    ListNode<T> *t1 = head, *t2;
+      while(t1 != nullptr){
+        t2 = t1->next;
+        delete t1;
+        t1 = t2;
+      }
+  }
+
+  void push_front(T val){
+    ListNode<T> *newNode = new ListNode<T>(val);
+    if(head == nullptr){
+      tail = head;
+    }else{
+      newNode->next = head;
+      head->prev = newNode;
     }
- 
+    head = newNode;
+    size++;
+  }
+  /*
+  void push_back(T val){
+    
+  }
+  */
     // 멤버 함수
     void insert(int k, T value){ // k번째 원소 앞에 새 원소 삽입
         try{
