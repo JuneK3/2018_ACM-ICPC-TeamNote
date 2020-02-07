2c6eff0be698168741899245d001df1bcda80f01
rlarudrms28 <rlarudrms28@gmail.com>
2020-02-07 06:49:08 +0000

    double_linked_list update

--- src/Data Structure/linked_list.cpp
+++ src/Data Structure/linked_list.cpp
@@ -1,19 +1,23 @@
-// 양방항 링크드 리스트로 구현
-// c++의 템플릿을 이용해서 stl처럼 사용할 수 있게 코딩
-// 멤버함수 : push_front / push_back / pop_front / pop_back /
-// insert / erase...
+// c++의 템플릿을 이용해서 stl처럼 사용할 수 있게 구현
+
+// push_front과 push_back 멤버함수 사용시
+// head와 tail 포인터의 위치가 갱신되지 않는 설계 오류 발생
+// 재설계 필요
+
+// <수정안>
+// 멤버함수 : insert / erase / changeData / print(head->tail)
+// 단일, 이중, 원형 링크드리스트 모두 따로 구현
+// insert 함수에서 head와 tail 포인터의 위치가 갱신되도록 구현
+// 참고 : https://meylady.tistory.com/5?category=784614
 
 #include <iostream>
-#include <cstdlib>
 using namespace std;
- 
-class InvalidIndexException{};
  
 template<typename T>
 class ListNode{
 public:
   T value;
-  ListNode<T> *next; // 마지막 노드면 nullptr
+  ListNode<T> *next;
   ListNode<T> *prev;
   // 생성자
   ListNode<T>(T value1, ListNode<T> *prev1, ListNode<T> *next1): value(value1), prev(prev1), next(next1){}
@@ -28,19 +32,13 @@ public:
   ListNode<T> *tail;
  
   // 생성자
+  LinkedList<T>(){
+    size = 0;
+    tail = head = nullptr;
+  }
   LinkedList<T>(T value){
     tail = head = new ListNode<T>(value);
     size = 1;
-  }
- 
-  // 소멸자
-  ~LinkedList<T>(){
-    ListNode<T> *t1 = head, *t2;
-      while(t1 != nullptr){
-        t2 = t1->next;
-        delete t1;
-        t1 = t2;
-      }
   }
 
   void push_front(T val){
@@ -54,33 +52,66 @@ public:
     head = newNode;
     size++;
   }
-  /*
+  
   void push_back(T val){
-    
-  }
-  */
-    // 멤버 함수
-    void insert(int k, T value){ // k번째 원소 앞에 새 원소 삽입
-        try{
-            // 예외 처리: 잘못된 인덱스
-            if(k < 0 || k > size) throw InvalidIndexException();
- 
-            if(k == 0){ // 처음에 삽입
-                head = new ListNode<T>(value, head);
-            }
-            else{ // k번째 원소 앞에 삽입
-                ListNode<T> *dest = head;
-                for(int i=0; i<k-1; i++) dest = dest->next;
-                dest->next = new ListNode<T>(value, dest->next);
-            }
-            size++;
-        }
-        catch(InvalidIndexException e){
-            cerr << "잘못된 인덱스입니다." << endl;
-            exit(1);
-        }
+    ListNode<T> *newNode = new ListNode<T>(val);
+    if(tail == nullptr){
+      head = tail;
+    }else{
+      newNode->prev = tail;
+      tail->next = newNode;
     }
- 
+    tail = newNode;
+    size++;
+  }
+  
+  void pop_front(){
+    if(head != nullptr){
+      ListNode<T> *remove = head;
+      head = head->next;
+      head->prev = nullptr;
+      delete remove;
+      size--;
+    }
+  }
+
+  void pop_back(){
+    if(tail != nullptr){
+      ListNode<T> *remove = tail;
+      tail = tail->prev;
+      tail->next = nullptr;
+      delete remove;
+      size--;
+    }
+  }
+
+  void insert(int idx, T val){
+    if(idx < 0 || idx >= size){
+      cout << "인덱스 범위 초과" << endl;
+      return;
+    }
+    else if(idx == 0){
+      push_front(val);
+    }
+    else if(idx == size-1){
+      push_back(val);
+    }
+    else{
+      ListNode<T> *newNode = new ListNode<T>(val);
+      ListNode<T> *cur = head;
+      int i=0;
+      while(i < idx - 1){
+        cur = cur->next;
+        i++;
+      }
+      newNode->prev = cur;
+      newNode->next = cur->next;
+      cur->next = newNode;
+      newNode->next->prev = newNode;
+      //cur->prev = newNode->prev;
+    }
+  }
+  /*
     void erase(int k){ // k번째 원소를 제거
         try{
             // 예외 처리: 잘못된 인덱스
@@ -105,13 +136,33 @@ public:
             exit(2);
         }
     }
- 
-    int search(T value){ // 값을 찾아 첫번째 인덱스 리턴, 없으면 -1 리턴
-        ListNode<T> *temp = head;
-        for(int i=0; i<size; i++){
-            if(temp->value == value) return i;
-            temp = temp->next;
-        }
-        return -1; // 못 찾음
+  */
+  int search(T value){ // 값을 찾아 첫번째 인덱스 리턴, 없으면 -1 리턴
+    ListNode<T> *temp = head;
+    for(int i=0; i<size; i++){
+      if(temp->value == value) return i;
+      temp = temp->next;
     }
+    return -1;
+  }
+
+  void print(){
+    ListNode<T> *cur = head;
+    while(cur != nullptr){
+      if(cur->next == nullptr){
+        cout << cur->value << '\n';
+      }else{
+        cout << cur->value << " ";
+      }
+      cur = cur->next;
+    }
+  }
+
+  // 소멸자
+  ~LinkedList<T>(){
+    ListNode<T> *cur = head;
+    while(cur != nullptr){
+      pop_front();
+    }
+  }
 };
