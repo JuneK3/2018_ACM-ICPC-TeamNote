e0be1c5c614f0c70d1490e6b539d0cdf274f1e5c
junek3 <rlarudrms28@gmail.com>
2020-08-20 02:25:45 +0900

    dfs bfs update

--- src/Search/bfs.cpp
+++ src/Search/bfs.cpp
@@ -1,29 +1,95 @@
 // 인접행렬 O(V^2)
 // 인접리스트 O(V+E)
 
-// 인접리스트를 이용한 구현
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> a[]; // 정점간 간선 저장
-bool check[]; // 방문한 정점 체크
+vector<int> adj[]; // 인접리스트
+int check[];       // 정점 방문 여부 체크
 
-void bfs(int start) {
+// 인럽리스트 구현
+void bfs(int start)
+{
+    check[start] = 1;
     queue<int> q;
-    check[start] = true;
     q.push(start);
-    while (!q.empty()) {
-        int node = q.front();
+    cout << start << ' ';
+    while (!q.empty())
+    {
+        int cur = q.front();
         q.pop();
-        printf("%d ",node); // 방문정점 출력
-        for (int i=0; i<a[node].size(); i++) {
-            int next = a[node][i];
-            if (check[next] == false) {
-                check[next] = true;
+        for (int i = 0; i < adj[cur].size(); i++)
+        {
+            int next = adj[cur][i];
+            if (check[next])
+                continue;
+            check[next] = 1;
+            q.push(next);
+            cout << next << ' ';
+        }
+    }
+}
+
+int adj[][]; // 인접행렬
+int check[];
+
+// 인접 행렬 구현
+void bfs(int start)
+{
+    check[start] = 1;
+    queue<int> q;
+    q.push(start);
+    cout << start << ' ';
+    while (!q.empty())
+    {
+        int cur = q.front();
+        q.pop();
+        for (int next = 1; next <= n; next++)
+        {
+            if (!check[next] && adj[cur][next] == 1)
+            {
+                check[next] = 1;
                 q.push(next);
+                cout << next << ' ';
+            }
+        }
+    }
+}
+
+// 격자판 버전
+int board[][]; // 격자판 값 저장
+int check[][]; // 방문여부 체크
+
+int dy[] = {0, 0, 1, -1};
+int dx[] = {1, -1, 0, 0};
+
+// 격자판의 가로 세로 범위를 초과하는지 검사
+bool isinRange(int y, int x)
+{
+    return (0 <= y && y <= n - 1) && (0 <= x && x <= m - 1);
+}
+
+// 격자판의 값이 1인 칸을 탐색
+void bfs(int y, int x)
+{
+    check[y][x] = true;
+    queue<pair<int, int>> q;
+    q.push({y, x});
+    while (!q.empty())
+    {
+        int curY = q.front().first;
+        int curX = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int ny = curY + dy[i];
+            int nx = curX + dx[i];
+            if (!isInRange(ny, nx))
+                continue;
+            if (!check[ny][nx] && board[ny][nx] == 1)
+            {
+                check[ny][nx] = 1;
+                q.push({ny, nx});
             }
         }
     }
--- src/Search/dfs.cpp
+++ src/Search/dfs.cpp
@@ -1,46 +1,126 @@
 // 인접행렬 O(V^2)
 // 인접리스트 O(V+E)
 
-// 인접리스트를 이용한 구현
-// 재귀를 이용한 구현
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <stack>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<int> a[]; // 정점간 간선 저장
-bool check[]; // 방문한 정점 체크
+vector<int> adj[]; // 인접리스트
+int check[];       // 방문한 정점 체크
 
-void dfs(int node) {
-    check[node] = true;
-    // printf("%d ",node);
-    for (int i=0; i<a[node].size(); i++) {
-        int next = a[node][i];
-        if (check[next] == false) {
+// 인접리스트 구현
+void dfs(int start)
+{
+    check[start] = true;
+    cout << start << ' ';
+    for (int i = 0; i < adj[start].size(); i++)
+    {
+        int next = adj[start][i];
+        if (check[next])
+            continue;
+        dfs(next);
+    }
+}
+
+int adj[][]; // 인접행렬
+int check[];
+
+// 인접행렬 구현
+void dfs(int start)
+{
+    check[start] = true;
+    cout << start << ' ';
+    for (int next = 1; next <= n; next++)
+    {
+        if (!check[next] && adj[start][next] == 1)
+        {
             dfs(next);
         }
     }
 }
-// 스택을 이용한 비재귀 구현
-void dfs(int node) {
-    stack<pair<int,int>> s;
-    s.push(make_pair(node,0));
-    check[node] = true;
-    printf("%d ",node); // 방문정점 출력
-    while (!s.empty()) {
-        int node = s.top().first;
-        int start = s.top().second;
-        s.pop();
-        for (int i=start; i<a[node].size(); i++) {
-            int next = a[node][i];
-            if (check[next] == false) {
-                printf("%d ",next);
-                check[next] = true;
-                s.push(make_pair(node,i+1));
-                s.push(make_pair(next,0));
+
+// 스택을 이용한 구현 (인접리스트)
+void dfs(int start)
+{
+    check[start] = true;
+    stack<int> st;
+    st.push(start);
+    cout << start << ' ';
+    while (!st.empty())
+    {
+        int cur = st.top();
+        bool can_go = false;
+        for (int i = 0; i < adj[cur].size(); i++)
+        {
+            int next = adj[cur][i];
+            if (check[next])
+                continue;
+            can_go = true;
+            check[next] = true;
+            st.push(next);
+            cout << next << ' ';
+            break;
+        }
+        if (!can_go)
+        {
+            st.pop();
+        }
+    }
+}
+
+// 스택을 이용한 구현 (인접행렬)
+void dfs(int start)
+{
+    check[start] = true;
+    stack<int> st;
+    st.push(start);
+    cout << start << ' ';
+    while (!st.empty())
+    {
+        int cur = st.top();
+        bool can_go = false;
+        for (int next = 1; next <= n; next++)
+        {
+            if (!check[next] && adj[cur][next] == 1)
+            {
+                can_go = true;
+                check[next] = 1;
+                st.push(next);
+                cout << next << ' ';
                 break;
             }
+        }
+        if (!can_go)
+        {
+            st.pop();
+        }
+    }
+}
+
+// 격자판 버전
+int board[][];
+int check[][];
+
+int dy[] = {0, 0, 1, -1};
+int dx[] = {1, -1, 0, 0};
+
+// 격자판의 가로 세로 범위 내에 있는지 검사
+bool isInRange(int y, int x)
+{
+    return (0 <= y && y <= n - 1) && (0 <= x && x <= m - 1);
+}
+
+void dfs(int y, int x)
+{
+    check[y][x] = 1;
+    for (int i = 0; i < 4; i++)
+    {
+        int ny = y + dy[i];
+        int nx = x + dx[i];
+        if (!isInRange(ny, nx))
+            continue;
+        if (!check[ny][nx] && board[ny][nx] == 1)
+        {
+            dfs(ny, nx);
         }
     }
 }
