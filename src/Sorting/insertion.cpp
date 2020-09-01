7a6af8b620e6341fd4ba2f26c2cec0920af06ccc
Junek3 <rlarudrms28@gmail.com>
2020-09-01 18:57:39 +0900

    Update insertion sort

---
+++ src/Sorting/insertion.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int> &A)
+{
+    for (int i = 0; i < A.size(); ++i)
+    {
+        // A[0..i-1] 에 A[i] 를 끼워넣는다
+        int j = i;
+        while (j > 0 && A[j - 1] > A[j])
+        {
+            swap(A[j - 1], A[j]);
+            --j;
+        }
+    }
+}
