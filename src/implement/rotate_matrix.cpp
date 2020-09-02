cd9ad49edc3f9f2e26692c7ced5e69b285885ddd
Junek3 <rlarudrms28@gmail.com>
2020-09-03 00:41:30 +0900

    Update rotate_matrix

--- src/implement/rotate_matrix.cpp
+++ src/implement/rotate_matrix.cpp
@@ -1,7 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> rotateMatrixBy90Degree(vector<vector<int>> a)
+// 배열을 시계방향으로 90도 회전
+// 반시계 방향으로 회전하는 경우는 시계방향으로 90도로 3번 회전시키는 것으로 구현 가능
+vector<vector<int>> rotate_Matrix(vector<vector<int>> a)
 {
     int n = a.size();                              // 행 길이 계산
     int m = a[0].size();                           // 열 길이 계산
